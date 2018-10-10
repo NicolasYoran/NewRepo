07d010b2144465c72e3f2818cbec4c3060675b79
Aluno <Aluno@DOSO121793>
2018-10-10 11:03:24 -0300

    final

---
+++ aula10_10_calculadora_simples/aula10_10_calculadora_simples/aula10_10_calculadora_simples.cpp
@@ -0,0 +1,16 @@
+// aula10_10_calculadora_simples.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+float soma(a + b)
+{
+	float primeiroNum;
+	float segundoNum;
+	printf("\rVamos somar dois números c:\n\n")
+	printf("\rDigite o primeiro número que deseja somar: %f")
+	scanf_s();
+	printf("\rDigite o segundo número que deseja somar: %f")
+
+}
